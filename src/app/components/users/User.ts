8378e0483fdf0525b30fb433e8be8335b846ae65
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2024-03-19 20:48:15 +0200

    UsersAddForm

---
+++ src/app/components/users/User.ts
@@ -0,0 +1,6 @@
+export interface User {
+    id: number,
+    name: String,
+    email: String,
+    phones: String[]
+}
---
+++ src/app/components/users/UsersData.ts
@@ -0,0 +1,16 @@
+import {User} from './User'
+
+export const users: User[] = [
+    {
+        id: 1,
+        name: "Hello",
+        email: "asd@gmail.com",
+        phones: ['38096222111'] 
+    },
+    {
+        id: 3,
+        name: "World",
+        email: "qwerty@gmail.com",
+        phones: ['380964444444', '111111111'] 
+    }
+]
